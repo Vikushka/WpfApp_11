011c12bbce8b7eda81627604ee1f1eeb90e92747
R2 <R2@MILA-SHKA>
2023-04-14 20:05:37 +0400

    Добавьте файлы проекта.

---
+++ WpfApp11/Forms/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox
+               .Show("Вы действительно хотите удалить этот файл?", "внимание!",
+               MessageBoxButton.YesNo,
+               MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("товар удален!");
+            }
+            else
+            {
+                MessageBox.Show("товар не удален!");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox
+               .Show("Добавить товар", "внимание!",
+               MessageBoxButton.YesNo,
+               MessageBoxImage.Question);
+            Window1 taskWindow = new Window1();
+            taskWindow.Show();
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("товар добавлен!");
+            }
+            else
+            {
+                MessageBox.Show("товар не добавлен!");
+            }
+        }
+
+       
+    }
+}
---
+++ WpfApp11/Window1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_generate_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(tb_content.Text, QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qR = new QRCode(qRCodeData);
+
+            Bitmap qr = qR.GetGraphic(150);
+
+            image_qrcode.Source = Convert(qr);
+
+        }
+        public BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
